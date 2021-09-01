830f1d88f53a65d1e2e6a626f752dc9f89a27f1e
Antony Gilo <giloantony@gmail.com>
2021-09-01 00:21:38 -0700

    Added solution

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point, prints "_putchar" to the standard output
- *
- * Return: Always 0
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
 	return (0);
 }
