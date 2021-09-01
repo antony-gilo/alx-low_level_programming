07bb924622ad69520d3c585cac78e4de09bb9fa7
Antony Gilo <giloantony@gmail.com>
2021-08-31 23:57:43 -0700

    Buggy Bug

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,18 +5,16 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar(10);
-	_putchar('\n');
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
 	return (0);
 }
